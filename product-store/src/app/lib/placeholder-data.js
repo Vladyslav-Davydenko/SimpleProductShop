d8b8f0df08ebb0ee5e6a9dd83e4b745438d07c24
Vladyslav <5vladik068@gmail.com>
2024-02-13 15:24:27 +0200

    Versel Postgres

---
+++ product-store/src/app/lib/placeholder-data.js
@@ -0,0 +1,44 @@
+const perfumes = [
+  {
+    url: "/perfumes/alfred-guzman-v6Rj0WB6F0I-unsplash.jpg",
+    title: "Alfred Guzman",
+    price: 300,
+    id: 1,
+  },
+  {
+    url: "/perfumes/jeroen-den-otter-2b0JeJTEclQ-unsplash.jpg",
+    title: "Jeroen Den Otter",
+    price: 300,
+    id: 2,
+  },
+  {
+    url: "/perfumes/laura-chouette-4sKdeIMiFEI-unsplash.jpg",
+    title: "Laura Chouette",
+    price: 300,
+    id: 3,
+  },
+  {
+    url: "/perfumes/marisa-garrido-3aql6Y9LKFo-unsplash.jpg",
+    title: "Marisa Garrido",
+    price: 300,
+    id: 4,
+  },
+  {
+    url: "/perfumes/laura-chouette-gbT2KAq1V5c-unsplash.jpg",
+    title: "Laura Chouette",
+    price: 300,
+    id: 5,
+  },
+  {
+    url: "/perfumes/vishal-banik-OhBmysUAjio-unsplash.jpg",
+    title: "Maxim Lozyanko",
+    price: 300,
+    id: 6,
+  },
+  {
+    url: "/perfumes/parth-natani-uqJdOfHGb-w-unsplash.jpg",
+    title: "Alfred Guzman",
+    price: 300,
+    id: 7,
+  },
+];
---
+++ product-store/src/scripts/seed.js
@@ -0,0 +1,60 @@
+const { db } = require("@vercel/postgres");
+const {
+  invoices,
+  customers,
+  revenue,
+  users,
+} = require("../app/lib/placeholder-data.js");
+
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS perfumes (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        url TEXT NOT NULL UNIQUE,
+        price NUMERIC NOT NULL
+      );
+    `;
+
+    console.log(`Created "perfumes" table`);
+
+    // Insert data into the "users" table
+    const insertedPerfumes = await Promise.all(
+      perfumes.map(async (perfume) => {
+        return client.sql`
+          INSERT INTO perfumes (id, title, url, price)
+          VALUES (${perfume.id}, ${perfume.title}, ${perfume.url}, ${perfume.price})
+          ON CONFLICT (id) DO NOTHING;
+        `;
+      })
+    );
+
+    console.log(`Seeded ${insertedPerfumes.length} perfumes`);
+
+    return {
+      createTable,
+      perfumes: insertedPerfumes,
+    };
+  } catch (error) {
+    console.error("Error seeding perfumes:", error);
+    throw error;
+  }
+}
+
+async function main() {
+  const client = await db.connect();
+
+  await seedPerfumes(client);
+
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error(
+    "An error occurred while attempting to seed the database:",
+    err
+  );
+});
